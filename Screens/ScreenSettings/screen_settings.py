5ea772f893bfcbe534e5c3aaf1c1a5acd064bfb1
Star <129488313+Bombel1234@users.noreply.github.com>
2024-10-13 16:06:03 +0200

    Add files via upload

---
+++ Screens/ScreenSettings/screen_settings.py
@@ -0,0 +1,337 @@
+from kivy.storage.jsonstore import JsonStore
+from kivymd.toast import toast
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.screen import MDScreen
+from kivymd.app import MDApp
+from kivymd.uix.menu import MDDropdownMenu
+from kivy.clock import Clock, mainthread
+import json
+from kivy.logger import Logger
+from os.path import exists
+from kivymd.uix.fitimage import FitImage
+from kivymd.toast.androidtoast import toast
+
+from androidstorage4kivy import SharedStorage, Chooser
+from android import mActivity
+
+from baza.create_baza_data import (
+    select_all_auto,
+    delete_auto_in_baza, or_table_in_baza,
+)
+
+primary_palette = [
+    'Blue', 'Pink', 'Purple', 'DeepPurple', 'Indigo', 'Red',
+    'LightBlue', 'Cyan', 'Teal', 'Green', 'LightGreen', 'Lime',
+    'Yellow', 'Amber', 'Orange', 'DeepOrange', 'Brown', 'Gray',
+    'BlueGray'
+]
+color_hue = [
+    '50', '100', '200', '300', '400', '500',
+    '600', '700', '800', '900', 'A100', 'A200', 'A400', 'A700'
+]
+
+
+class ScreenSettings(MDScreen):
+    index = 0
+    index_hue = 0
+    dialog = None
+    menu = None
+    click_language = True
+    list_from_json_pl = []
+    list_from_json_ukr = []
+    app = MDApp.get_running_app()
+    text_dialog = ''
+    text_dialog1 = ''
+    text_dialog2 = ''
+    text_dialog3 = ''
+    text_dialog4 = ''
+    text_dialog5 = ''
+    path = None
+    shared = None
+    key = ''
+    img = None
+    res_marka = ''
+    res_model = ''
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.chooser = Chooser(self.chooser_callback)
+        self.img = FitImage()
+
+    def on_enter(self, *args):
+        Clock.schedule_once(self.active_switch, 0)
+        Clock.schedule_once(self.open_json, 0)
+        Clock.schedule_once(self.open_json_for_name_auto, 1)
+    
+    def on_leave(self, *args):
+        sp = []
+        root = self.ids.box_photo
+        self.ids.btn_photo.disabled = False
+        for child in root.children:
+            sp.append(child)
+        if len(sp) > 0:   
+            self.ids.box_photo.remove_widget(self.img)
+
+    def open_json_for_name_auto(self, *args):
+        if exists('main.json'):
+            store_read = JsonStore('main.json')
+            self.res_marka = store_read.store_get('app')['marka']
+            self.res_model = store_read.store_get('app')['model']
+            self.key = self.res_marka + self.res_model
+            
+
+    def open_json(self, *args):
+        with open('language.json', encoding='utf-8') as file:
+            data = json.load(file)
+        store = JsonStore('settings.json')
+        res_language = store.store_get('language')['select_language']
+        res_pl = data["screen_settings"]["pl"]
+        res_ukr = data["screen_settings"]["ukr"]
+        if res_language == 'pl':
+            self.text_dialog = res_pl['t4']
+            self.text_dialog1 = res_pl['t5']
+            self.text_dialog2 = res_pl['t6']
+            self.text_dialog3 = res_pl['t7']
+            self.text_dialog4 = res_pl['t8']
+            self.text_dialog5 = res_pl['t10']
+            self.ids.btn_delete.text = res_pl['t3']
+            self.ids.text_toolbar.title = res_pl['t1']
+            self.ids.btn_photo.text = res_pl['t9']
+        else:
+            self.text_dialog = res_ukr['t4']
+            self.text_dialog1 = res_ukr['t5']
+            self.text_dialog2 = res_ukr['t6']
+            self.text_dialog3 = res_ukr['t7']
+            self.text_dialog4 = res_ukr['t8']
+            self.text_dialog5 = res_ukr['t10']
+            self.ids.btn_delete.text = res_ukr['t3']
+            self.ids.text_toolbar.title = res_ukr['t1']
+            self.ids.btn_photo.text = res_ukr['t9']
+
+    def active_switch(self, *args):
+        store = JsonStore('settings.json')
+        res_language = store.store_get('language')['select_language']
+        res_theme = store.store_get('theme')['theme']
+        res_primary_palette = store.store_get('palette')['primary_palette']
+        res_primary_hue = store.store_get('hue')['primary_hue']
+        self.ids.color_hue.text = res_primary_hue
+        self.ids.color_primary.text = res_primary_palette
+        if res_language == 'pl':
+            self.ids.switch_language.active = False
+            self.ids.label_pl.text_color = 'red'
+
+        else:
+            self.ids.switch_language.active = True
+            self.ids.label_ukr.text_color = 'red'
+
+        if res_theme == 'Light':
+            self.ids.switch_theme.active = False
+            self.ids.light.text_color = 'red'
+        else:
+            self.ids.switch_theme.active = True
+            self.ids.dark.text_color = 'red'
+
+    def back_screen_logo(self, obj):
+        app = MDApp.get_running_app()
+        app.sm.current = 'screen_logo'
+
+    def click_plus(self):
+        app = MDApp.get_running_app()
+        store = JsonStore('settings.json', indent=True)
+        if self.index < len(primary_palette) - 1:
+            self.index += 1
+        app.theme_cls.primary_palette = primary_palette[self.index]
+        self.ids.color_primary.text = primary_palette[self.index]
+        store.put('palette', primary_palette=primary_palette[self.index])
+
+    def click_minus(self):
+        app = MDApp.get_running_app()
+        store = JsonStore('settings.json', indent=True)
+        if self.index > 0:
+            self.index -= 1
+        app.theme_cls.primary_palette = primary_palette[self.index]
+        self.ids.color_primary.text = primary_palette[self.index]
+        store.put('palette', primary_palette=primary_palette[self.index])
+
+    def click_plus_hue(self):
+        app = MDApp.get_running_app()
+        store = JsonStore('settings.json', indent=True)
+        if self.index_hue < len(color_hue) - 1:
+            self.index_hue += 1
+        app.theme_cls.primary_hue = color_hue[self.index_hue]
+        self.ids.color_hue.text = color_hue[self.index_hue]
+        store.put('hue', primary_hue=color_hue[self.index_hue])
+
+    def click_minus_hue(self):
+        app = MDApp.get_running_app()
+        store = JsonStore('settings.json', indent=True)
+        if self.index_hue > 0:
+            self.index_hue -= 1
+        app.theme_cls.primary_hue = color_hue[self.index_hue]
+        self.ids.color_hue.text = color_hue[self.index_hue]
+        store.put('hue', primary_hue=color_hue[self.index_hue])
+
+    def language_select(self, switch, value):
+        store = JsonStore('settings.json', indent=True)
+
+        with open('language.json', encoding='utf-8') as file:
+            data = json.load(file)
+        res_ukr = data["screen_settings"]["ukr"]
+        res_pl = data["screen_settings"]["pl"]
+        if value:
+            store.put('language', select_language='ukr')
+            self.ids.label_pl.text_color = 'black'
+            self.ids.label_ukr.text_color = 'red'
+            self.ids.text_toolbar.title = res_ukr['t1']
+            self.ids.lab1.text = res_ukr['t2']
+            self.ids.btn_delete.text = res_ukr['t3']
+            self.ids.btn_photo.text = res_ukr['t9']
+            self.text_dialog = res_ukr['t4']
+            self.text_dialog1 = res_ukr['t5']
+            self.text_dialog2 = res_ukr['t6']
+            self.text_dialog3 = res_ukr['t7']
+            self.text_dialog4 = res_ukr['t8']
+            self.text_dialog5 = res_ukr['t10']
+        else:
+            store.put('language', select_language='pl')
+            self.ids.label_ukr.text_color = 'black'
+            self.ids.label_pl.text_color = 'red'
+            self.ids.text_toolbar.title = res_pl['t1']
+            self.ids.lab1.text = res_pl['t2']
+            self.ids.btn_delete.text = res_pl['t3']
+            self.ids.btn_photo.text = res_pl['t9']
+            self.text_dialog = res_pl['t4']
+            self.text_dialog1 = res_pl['t5']
+            self.text_dialog2 = res_pl['t6']
+            self.text_dialog3 = res_pl['t7']
+            self.text_dialog4 = res_pl['t8']
+            self.text_dialog5 = res_pl['t10']
+
+    def theme_select(self, switch, value):
+        store = JsonStore('settings.json', indent=True)
+        app = MDApp.get_running_app()
+        if value:
+            store.put('theme', theme='Dark')
+            app.theme_cls.theme_style_switch_animation = True
+            app.theme_cls.theme_style_switch_animation_duration = 0.8
+            app.theme_cls.theme_style = 'Dark'
+            self.ids.dark.text_color = 'red'
+            self.ids.light.text_color = 'white'
+        else:
+            store.put('theme', theme='Light')
+            app.theme_cls.theme_style_switch_animation = True
+            app.theme_cls.theme_style_switch_animation_duration = 0.8
+            app.theme_cls.theme_style = 'Light'
+            self.ids.light.text_color = 'red'
+            self.ids.dark.text_color = 'black'
+
+    def click_delete_auto(self, item):
+        if or_table_in_baza():
+            list_auto = select_all_auto()
+            if len(list_auto) > 0:
+                menu_items = [
+                    {
+                        "text": f"{i[0]}  {i[1]}",
+                        "viewclass": "OneLineListItem",
+                        "on_release": lambda x=i: self.menu_list_auto(x),
+                    } for i in list_auto
+                ]
+                self.menu = MDDropdownMenu(
+                    caller=item,
+                    items=menu_items,
+                    width_mult=4,
+                )
+                self.menu.caller.pos = [10, 400]
+                self.menu.open()
+            else:
+                MDDialog(title=f"{self.text_dialog}").open()
+        else:
+            MDDialog(title=f"{self.text_dialog}").open()
+
+    def menu_list_auto(self, name_auto):
+        self.select_auto_delete(name_auto)
+
+    def cancel_dialog(self, obj):
+        self.dialog.dismiss()
+        self.menu.dismiss()
+
+    def select_auto_delete(self, select_auto):
+        marka = select_auto[0]
+        model = select_auto[1]
+        self.dialog = MDDialog(
+            title=f'{self.text_dialog1} {marka} {model}  {self.text_dialog2}',
+            buttons=[
+                MDFlatButton(
+                    text="CANCEL",
+                    theme_text_color="Custom",
+                    text_color=self.theme_cls.primary_color,
+                    on_release=self.cancel_dialog
+                ),
+                MDFlatButton(
+                    text="OK",
+                    theme_text_color="Custom",
+                    md_bg_color='red',
+                    text_color='white',
+                    on_release=lambda x: self.delete_auto(select_auto)
+                ),
+            ],
+        )
+        self.dialog.open()
+
+    def delete_auto(self, name_auto):
+        name_auto = f"{name_auto[0].lower()}{name_auto[1].lower()}"
+        delete_auto_in_baza(name_auto)
+        self.dialog.dismiss()
+        self.menu.dismiss()
+        toast(f"{self.text_dialog3}")
+        store = JsonStore('main.json', indent=True)
+        store.put('app', marka="", model="")
+
+        store_del = JsonStore('main.json')
+        res_marka = store_del.store_get('app')['marka']
+        res_model = store_del.store_get('app')['model']
+        key = res_marka.lower() + res_model.lower()
+        store_del.put(key, name_photo='')
+        
+
+#################################################3
+    def open_closer(self):
+        if exists('main.json'):
+            self.chooser.choose_content("image/*")
+        else:
+            dialog = MDDialog(title=self.text_dialog5)
+            dialog.open()
+
+    def chooser_callback(self,uri_list):
+        try:
+            ss = SharedStorage()
+            for uri in uri_list:
+                self.path = ss.copy_from_shared(uri)
+                if self.path:
+                    self.shared = ss.copy_to_shared(self.path)
+                              
+            self.display()
+        except Exception as e:
+            pass
+    @mainthread
+    def display(self):
+        self.name_file = str(self.path).split('/')[-1]
+        self.img.source = self.path
+        self.ids.box_photo.add_widget(self.img)
+        self.ids.btn_photo.disabled = True
+        toast(self.text_dialog4, True, gravity=40)
+
+        context = mActivity.getApplicationContext()
+        result = context.getExternalFilesDir(None) 
+        
+        
+        if result:
+            storage_path =  str(result.toString())
+            store = JsonStore(f'{storage_path}/img.json')
+            store.put(self.key,name_file=self.name_file)
+            
+            
+        
+        
+
+
---
+++ baza/create_baza_data.py
@@ -0,0 +1,121 @@
+import sqlite3
+from android import mActivity
+
+
+
+LIST_AUTO = 'list_auto'
+
+
+
+def create_baza():
+    context = mActivity.getApplicationContext()
+    result =  context.getExternalFilesDir(None)   # don't forget the argument
+    if result:
+        storage_path =  str(result.toString())
+        conn = sqlite3.connect(f'{storage_path}/my_sql.db')
+        cur = conn.cursor()
+        
+    
+    return conn, cur
+
+
+connect, cursor = create_baza()
+
+
+
+def create_table_list_auto():
+    cursor.execute(
+        f"CREATE TABLE IF NOT EXISTS {LIST_AUTO} ("
+        f"marka TEXT NOT NULL,"
+        f"marka_model NOT NULL,"
+        f"model TEXT NOT NULL)"
+    )
+    connect.commit()
+
+
+# чи е таблицi в базi
+def or_table_in_baza():
+    if cursor.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchone():
+        return True
+    return False
+
+
+# чи пуста  таблиця LIST_AUTO
+def is_one_rows_table() -> bool:
+    if cursor.execute(f"SELECT marka FROM {LIST_AUTO}").fetchone():
+        return True
+    return False
+
+
+# вивести все авта в list auto
+def select_all_auto():
+    res = cursor.execute(f"SELECT marka, model FROM {LIST_AUTO}").fetchall()
+    return res
+
+
+def or_ist_auto(marka_model) -> bool:
+    if cursor.execute(f"SELECT marka_model FROM {LIST_AUTO} WHERE marka_model='{marka_model}'").fetchone():
+        return True
+    return False
+
+
+# додавання автомобiля до бази
+def add_marka_model_baza(marka, title, model) -> None:
+    cursor.execute(f"INSERT INTO {LIST_AUTO} VALUES(?,?,?)",
+                   (marka, title, model))
+
+    connect.commit()
+
+
+# function table info-------##########
+def create_table_name_auto(name_auto):
+    cursor.execute(
+        f"CREATE TABLE IF NOT EXISTS {name_auto} ("
+        f"km INT NOT NULL,"
+        f"work TEXT NOT NULL,"
+        f"cena TEXT NOT NULL,"
+        f"data INT NOT NULL)"
+    )
+    connect.commit()
+
+
+def is_km(name_auto):
+    if cursor.execute(f"SELECT km FROM {name_auto}").fetchone():
+        return True
+    return False
+
+
+def digit_end_km(name_auto):
+    res = cursor.execute(f"SELECT km FROM {name_auto}").fetchall()
+    max_number = max(res)
+    return max_number
+
+
+# додавання iнформаций про авто
+def save_info_auto(km, work, cena, data, name_auto):
+    cursor.execute(f"INSERT INTO {name_auto} VALUES(?,?,?,?)",
+                   (km, work, cena, data))
+    connect.commit()
+
+
+# вибор всех записей об определеным авто
+def select_info_auto(name_auto) -> list:
+    res = cursor.execute(f"SELECT * FROM {name_auto}").fetchall()
+    return res
+
+
+def select_rowid_for_update(name_auto, km) -> int:
+    res = cursor.execute(f"SELECT ROWID FROM {name_auto} WHERE km={km}").fetchone()
+    return res[0]
+
+
+def update_baza_info(km, work, cena, data, name_auto, rowid) -> None:
+    cursor.execute(f"UPDATE {name_auto} SET km = {km}, work = '{work}', "
+                   f"cena = {cena}, data = '{data}' WHERE ROWID={rowid}")
+    connect.commit()
+
+
+def delete_auto_in_baza(name_auto):
+    cursor.execute(f"DROP TABLE {name_auto}")
+    cursor.execute(f"DELETE FROM {LIST_AUTO} WHERE marka_model='{name_auto}'")
+    connect.commit()
